511350b574b2c0b07649ded124860961adae765b
esentis <52862600+esentis@users.noreply.github.com>
2026-03-30 20:04:07 +0300

    🔧 Fix string extension method to correctly handle index bounds and update documentation example

--- lib/src/helpers/extensions.dart
+++ lib/src/helpers/extensions.dart
@@ -6,7 +6,7 @@ extension HelperExtensions on String? {
   /// ```dart
   /// String foo1 = 'esentis';
   /// String char1 = foo1.charAt(0); // returns 'e'
-  /// String char2 = foo1.charAt(4); // returns 'n'
+  /// String char2 = foo1.charAt(4); // returns 't'
   /// String? char3 = foo1.charAt(-20); // returns null
   /// String? char4 = foo1.charAt(20); // returns null
   /// ```
@@ -17,12 +17,9 @@ extension HelperExtensions on String? {
     if (this!.isEmpty) {
       return this;
     }
-    if (index > this!.length) {
+    if (index < 0 || index >= this!.length) {
       return null;
     }
-    if (index < 0) {
-      return null;
-    }
-    return this!.split('')[index];
+    return this![index];
   }
 }
